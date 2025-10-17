2ceda97bae7e798e3c8e0f7ce8a38801e766661e
Wisam851 <wisam@bidecsol.com>
2025-10-17 11:34:10 -0700

    trying to fixed the login screen ui

--- lib/features/auth/presentation/pages/login_screen.dart
+++ lib/features/auth/presentation/pages/login_screen.dart
@@ -18,6 +18,7 @@ class LoginScreen extends StatelessWidget {
         children: [
           // Background image
           Image.asset('assets/images/login_bg.png', fit: BoxFit.cover),
+          // Image.asset('assets/images/login_bg_front.png', fit: BoxFit.cover),
 
           // White gradient overlay
           Positioned(
@@ -31,10 +32,23 @@ class LoginScreen extends StatelessWidget {
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                   colors: [
-                    Colors.white.withOpacity(0.0),
-                    Colors.white.withOpacity(0.75),
+                    const Color.fromARGB(0, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(0, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(0, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(0, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(0, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(0, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(100, 255, 255, 255).withOpacity(0.0),
+                    const Color.fromARGB(250, 255, 255, 255).withOpacity(0.0),
+                    Colors.white.withOpacity(0.1),
+                    Colors.white.withOpacity(0.3),
+                    Colors.white.withOpacity(0.5),
+                    Colors.white.withOpacity(0.7),
                     Colors.white.withOpacity(0.8),
-                    Colors.white.withOpacity(0.85),
+                    Colors.white.withOpacity(0.9),
+                    Colors.white.withOpacity(0.9),
+                    Colors.white,
+                    Colors.white,
                     Colors.white,
                   ],
                 ),
@@ -46,7 +60,8 @@ class LoginScreen extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: Container(
               width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              height: size.height * 0.35,
+              padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 12),
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(0.95),
                 // borderRadius: const BorderRadius.only(
@@ -62,77 +77,79 @@ class LoginScreen extends StatelessWidget {
                 // ],
               ),
               child: SafeArea(
-                child: Column(
-                  mainAxisSize:
-                      MainAxisSize.min, // Space between top and bottom
-                  children: [
-                    // Top back button + title
-
-                    // // Bottom login form
-                    // Container(
-                    //   width: double.infinity,
-                    //   padding: const EdgeInsets.all(16),
-                    //   // margin: EdgeInsets.only(bottom: size.height * 0.05), // 5% bottom space
-                    //   decoration: BoxDecoration(
-                    //     color: Colors.white.withOpacity(0.85),
-                    //     borderRadius: BorderRadius.circular(12),
-                    //     boxShadow: [
-                    //       BoxShadow(
-                    //         color: Colors.black12,
-                    //         blurRadius: 8,
-                    //         offset: const Offset(0, 2),
-                    //       ),
-                    //     ],
-                    //   ),
-                    //   child: Column(
-                    //     mainAxisSize: MainAxisSize.min, // Wrap only its content
-                    //     children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      controller: controller.emailController,
-                    ),
-                    const SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      controller: controller.passwordController,
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.lightBlue[300],
-                          shape: RoundedRectangleBorder(
+                child: Center(
+                  child: Column(
+                    mainAxisSize:
+                        MainAxisSize.min, // Space between top and bottom
+                    children: [
+                      // Top back button + title
+                  
+                      // // Bottom login form
+                      // Container(
+                      //   width: double.infinity,
+                      //   padding: const EdgeInsets.all(16),
+                      //   // margin: EdgeInsets.only(bottom: size.height * 0.05), // 5% bottom space
+                      //   decoration: BoxDecoration(
+                      //     color: Colors.white.withOpacity(0.85),
+                      //     borderRadius: BorderRadius.circular(12),
+                      //     boxShadow: [
+                      //       BoxShadow(
+                      //         color: Colors.black12,
+                      //         blurRadius: 8,
+                      //         offset: const Offset(0, 2),
+                      //       ),
+                      //     ],
+                      //   ),
+                      //   child: Column(
+                      //     mainAxisSize: MainAxisSize.min, // Wrap only its content
+                      //     children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8),
                           ),
-                          elevation: 2,
-                          padding: const EdgeInsets.symmetric(vertical: 14),
                         ),
-                        onPressed: controller.login,
-                        child: const Text('Submit'),
+                        controller: controller.emailController,
                       ),
-                    ),
-                    const SizedBox(height: 12),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Forgot Password ?'),
-                    ),
-                  ],
+                      const SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        controller: controller.passwordController,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.lightBlue[300],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            elevation: 2,
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                          ),
+                          onPressed: controller.login,
+                          child: const Text('Submit'),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('Forgot Password ?'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
